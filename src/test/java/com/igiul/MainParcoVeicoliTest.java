ce34a1725b49af2f042427172d37570c2cba2c50
luigi Ritacco <luigiritacco1296@gmail.com>
2025-05-30 12:22:02 +0200

    ParcoVeicoli

---
+++ src/test/java/com/igiul/MainParcoVeicoliTest.java
@@ -0,0 +1,20 @@
+package com.igiul;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for MainParcoVeicoli.
+ */
+class MainParcoVeicoliTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testMainParcoVeicoli() {
+        assertEquals(1, 1);
+    }
+
+}
